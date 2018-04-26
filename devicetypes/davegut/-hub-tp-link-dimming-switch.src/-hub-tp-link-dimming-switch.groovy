db448ec188b92990dd8f9724a7b2cc58665bf04c
David Gutheinz <djgutheinz@outlook.com>
2018-04-26 11:52:30 -0500

    Updated default code and created new device handler for the HS220 Dimming Switch.

--- devicetypes/davegut/-hub-tp-link-dimming-switch.src/-hub-tp-link-dimming-switch.groovy
+++ devicetypes/davegut/-hub-tp-link-dimming-switch.src/-hub-tp-link-dimming-switch.groovy
@@ -27,11 +27,11 @@ TP-Link devices; primarily various users on GitHub.com.
 		b.	User file-internal selection of Energy Monitor
 			function enabling.
 	===== Plug/Switch Type.  DO NOT EDIT ====================*/
-	def deviceType = "Plug-Switch"			//	Plug/Switch
-//	def deviceType = "Dimming Switch"		//	HS220 Only
+//	def deviceType = "Plug-Switch"			//	Plug/Switch
+	def deviceType = "Dimming Switch"		//	HS220 Only
 //	===== Hub or Cloud Installation =========================*/
-	def installType = "Cloud"
-	//def installType = "Hub"
+//	def installType = "Cloud"
+	def installType = "Hub"
 //	===========================================================
 
 metadata {
